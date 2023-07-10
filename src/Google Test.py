50fda013e532214b19bb3c8fafd3a5d6b59d2430
glowzinthedark69 <75811554+glowzinthedark69@users.noreply.github.com>
2023-07-10 08:44:26 -0600

    Adding file and more learning.

---
+++ src/Google Test.py
@@ -0,0 +1,72 @@
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+#
+#
+# def test_website():
+#     # Create a new instance of the Chrome driver.
+#     driver = webdriver.Chrome()
+#
+#     # Open the website we want to test.
+#     driver.get("https://www.google.com/")
+#
+#     # Find the search bar and enter a search term.
+#     search_bar = driver.find_element(By.NAME, "q")
+#     search_bar.send_keys("Selenium")
+#
+#     # Wait for the search button to be clickable
+#     time.sleep(5)
+#
+#     # Click the search button.
+#     search_button = driver.find_element(By.NAME, "btnK")
+#     search_button.click()
+#
+#     # Wait for the search results to load.
+#     time.sleep(3)
+#
+#     # Check that the search results page contains the search term.
+#     assert "Selenium" in driver.page_source
+#
+#     # Close the driver.
+#     driver.quit()
+#
+#
+# if __name__ == "__main__":
+#     test_website()
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+
+def test_website():
+    # Create a new instance of the Chrome driver.
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+
+    # Open the website we want to test.
+    driver.get("https://www.google.com/")
+
+    # Find the search bar and enter a search term.
+    search_bar = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "q")))
+    search_bar.send_keys("Selenium")
+
+    # Click the search button. Google's search can be triggered by hitting the ENTER key.
+    search_bar.send_keys(Keys.RETURN)
+    print(driver.current_url)
+
+    # Wait for the search results to load.
+    time.sleep(3)
+
+    # Check that the search results page contains the search term.
+    assert "Selenium" in driver.page_source
+
+    # Close the driver.
+    driver.quit()
+
+
+if __name__ == "__main__":
+    test_website()
