4817e044749b7949f65a31ad1a4696a526f225fd
glowzinthedark69 <75811554+glowzinthedark69@users.noreply.github.com>
2023-08-20 09:38:35 -0600

    More practice with page object model

--- src/Login_Sample_Test.py
+++ src/Login_Sample_Test.py
@@ -13,6 +13,9 @@ class BaseTest(unittest.TestCase):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
 
+    def tearDown(self):
+        self.driver.quit()
+
 
 class TestLoginFunction(BaseTest):
 
@@ -28,56 +31,60 @@ class TestLoginFunction(BaseTest):
 
         # Submit the form
         self.test_page.submit_form()
-
-    def test_login(self):
-        driver = webdriver.Chrome()  # corrected line
-        # maximize window
-        driver.maximize_window()
-        # open the website
-        driver.get("https://practicetestautomation.com/practice-test-login/")
-
-        # locating username field and keying in the username
-        username = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "username"))
-        )
-        username.send_keys("student")
-
-        # locating password field and keying in the password
-        password = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "password"))
-        )
-        password.send_keys("Password123")
-
-        # locating login button and clicking it to login
-        login_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "submit"))
-        )
-        login_button.click()
-
-        # Pause the execution of the script for 5 seconds
         time.sleep(5)
 
-        # Check if the login was successful by validating the presence of a logout button
-        logout_link = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.LINK_TEXT, "Log out"))
-        )
+    def test_login_success(self):
+        self.test_page.validate_successful_login()
 
-        # Assert that the logout button is displayed, meaning a successful login
-        assert logout_link.is_displayed()
-
-        # Attempt to locate the text element
-        try:
-            element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located(
-                    (
-                        By.XPATH,
-                        "//*[text()='Congratulations student. You successfully logged in!']",
-                    )
-                )
-            )
-        except TimeoutException:
-            driver.quit()
-            raise Exception("The expected text was not found on the page")
+        # def test_login(self):
+        #     driver = webdriver.Chrome()  # corrected line
+        #     # maximize window
+        #     driver.maximize_window()
+        #     # open the website
+        #     driver.get("https://practicetestautomation.com/practice-test-login/")
+        #
+        #     # locating username field and keying in the username
+        #     username = WebDriverWait(driver, 10).until(
+        #         EC.element_to_be_clickable((By.ID, "username"))
+        #     )
+        #     username.send_keys("student")
+        #
+        #     # locating password field and keying in the password
+        #     password = WebDriverWait(driver, 10).until(
+        #         EC.element_to_be_clickable((By.ID, "password"))
+        #     )
+        #     password.send_keys("Password123")
+        #
+        #     # locating login button and clicking it to login
+        #     login_button = WebDriverWait(driver, 10).until(
+        #         EC.element_to_be_clickable((By.ID, "submit"))
+        #     )
+        #     login_button.click()
+        #
+        #     # Pause the execution of the script for 5 seconds
+        #     time.sleep(5)
+        #
+        #     # Check if the login was successful by validating the presence of a logout button
+        #     logout_link = WebDriverWait(driver, 10).until(
+        #         EC.element_to_be_clickable((By.LINK_TEXT, "Log out"))
+        #     )
+        #
+        #     # Assert that the logout button is displayed, meaning a successful login
+        #     assert logout_link.is_displayed()
+        #
+        #     # Attempt to locate the text element
+        #     try:
+        #         element = WebDriverWait(driver, 10).until(
+        #             EC.presence_of_element_located(
+        #                 (
+        #                     By.XPATH,
+        #                     "//*[text()='Congratulations student. You successfully logged in!']",
+        #                 )
+        #             )
+        #         )
+        #     except TimeoutException:
+        #         driver.quit()
+        #         raise Exception("The expected text was not found on the page")
 
         # close the driver
         driver.quit()
--- src/PageObjects/Login_Sample_Test_Objects.py
+++ src/PageObjects/Login_Sample_Test_Objects.py
@@ -9,15 +9,27 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
         self.url = "https://practicetestautomation.com/practice-test-login/"
-        self.user_name_field = (By.ID, "userName")
+        self.user_name_field = (By.ID, "username")
         self.user_password_field = (By.ID, "password")
         self.submit_button = (By.ID, "submit")
+        self.login_message = (By.XPATH,
+                              "//*[text()='Congratulations student. You successfully logged in!']")
+        self.logout_button = (By.LINK_TEXT, "Log out")
 
     def open(self):
         self.driver.get(self.url)
 
     def fill_form(self, user_name, user_password):
+        # Wait for the 'userName' field to be present before interacting with it
+        WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located(self.user_name_field)
+        )
         self.driver.find_element(*self.user_name_field).send_keys(user_name)
+
+        # Similarly, wait for the 'password' field
+        WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located(self.user_password_field)
+        )
         self.driver.find_element(*self.user_password_field).send_keys(user_password)
 
     def submit_form(self):
@@ -26,27 +38,26 @@ class LoginPage:
         )
         submit_button.click()
 
-    def form_success_validation(self):
-        output = WebDriverWait(self.driver, 5).until(
-            EC.presence_of_element_located(self.output_validation)
-        )
-        name_validation = output.find_element(*self.name_validation_locator)
-        email_validation = output.find_element(*self.email_validation_locator)
-        # Extract other elements if needed...
+    def get_login_success_message(self):
+        return WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(self.login_message))
 
-        return {
-            "name": name_validation.text,
-            "email": email_validation.text
-            # Add other extracted data if needed...
-        }
+    def get_logout_button(self):
+        return WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(self.logout_button))
 
-    def form_error_validation(self):
-        email_error = WebDriverWait(self.driver, 5).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "#userEmail.field-error"))
-        )
-        border_color = email_error.value_of_css_property("border-color")
-        return {
-            "error_element": email_error,
-            "border_color": border_color
-        }
+    def validate_successful_login(self):
+        # Check if the login message is displayed
+        assert self.get_login_success_message().is_displayed(), "Login success message not displayed"
+
+        # Check if the logout button is displayed
+        assert self.get_logout_button().is_displayed(), "Logout button not displayed"
+
+    # def form_error_validation(self):
+    #     email_error = WebDriverWait(self.driver, 5).until(
+    #         EC.presence_of_element_located((By.CSS_SELECTOR, "#userEmail.field-error"))
+    #     )
+    #     border_color = email_error.value_of_css_property("border-color")
+    #     return {
+    #         "error_element": email_error,
+    #         "border_color": border_color
+    #     }
     # In your test class:
